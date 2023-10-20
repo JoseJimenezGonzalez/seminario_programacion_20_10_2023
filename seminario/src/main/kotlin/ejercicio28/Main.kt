c9bb287508037e3d2e1461d14cf583fe2c18181c
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-10-20 21:29:58 +0200

    commit ejercicio28

--- seminario/src/main/kotlin/ejercicio28/Main.kt
+++ seminario/src/main/kotlin/ejercicio28/Main.kt
@@ -1,10 +1,17 @@
 package ejercicio28
 //Crea una función que determine si una cadena de texto contiene solo
 //caracteres alfabéticos (letras) y espacios en blanco.
-fun main(){
+fun main() {
+    val texto1 = "Hola Mundo"
+    val texto2 = "12345"
+    val texto3 = "Texto con números: 42"
 
+    println("Texto 1: ${contieneSoloLetrasYEspacios(texto1)}")
+    println("Texto 2: ${contieneSoloLetrasYEspacios(texto2)}")
+    println("Texto 3: ${contieneSoloLetrasYEspacios(texto3)}")
 }
 
-fun esSoloCondicion(){
-
+fun contieneSoloLetrasYEspacios(cadena: String): Boolean {
+    val patron = Regex("^[A-Za-z ]+\$")
+    return patron.matches(cadena)
 }
