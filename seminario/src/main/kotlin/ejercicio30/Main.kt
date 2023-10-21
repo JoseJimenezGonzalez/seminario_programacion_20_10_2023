fe7dc9f7c00fcde067a8cd11d2a8bf2db285a330
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-10-21 18:11:16 +0200

    commit ok

--- seminario/src/main/kotlin/ejercicio30/Main.kt
+++ seminario/src/main/kotlin/ejercicio30/Main.kt
@@ -2,19 +2,13 @@ package ejercicio30
 //Crea una función que, dado un número entero, devuelva True si es un número triangular
 //(puede representarse como un triángulo equilátero de puntos), o False en caso contrario.
 fun main(){
-    val numeroPrueba = 21
-    println("¿El numero $numeroPrueba es triangular? ${isTriangular(numeroPrueba)}")
+    val numeroPrueba1 = 20 //No es triangular
+    val numeroPrueba2 = 21 //Si es triangular
+    println("¿El numero $numeroPrueba1 es triangular? ${isTriangular(numeroPrueba1)}")
+    println("¿El numero $numeroPrueba2 es triangular? ${isTriangular(numeroPrueba2)}")
 }
 fun isTriangular(numero: Int): Boolean{
-    var numeroSerie = 0
-    var chivato = 0
-    var res = false
-    while(chivato < numero){
-        chivato = numeroSerie*(numeroSerie + 1)/2
-        if(chivato == numero){
-            res = true
-        }
-        numeroSerie++
-    }
-    return res
+    val solucion = ((-1) + Math.sqrt(1 + 8 * numero.toDouble())) / 2
+    return solucion.toInt().toDouble() == solucion
 }
+
--- seminario/src/main/kotlin/ejercicio32/Main.kt
+++ seminario/src/main/kotlin/ejercicio32/Main.kt
@@ -1,11 +1,15 @@
 package ejercicio32
-
+//Crear un Diccionario a partir de Listas
+//Escribe una función que tome dos listas, una lista de claves y otra lista de valores, y
+//cree un diccionario utilizando mapOf para combinar las listas en un diccionario
+//clave-valor. La función debe devolver el diccionario resultante.
 fun main(){
     val listaClave = listOf<Int>(1 ,2, 3)
     val listaValor = listOf<String>("Uno", "Dos", "Tres")
-    var mapa = fabricarDiccionario(listaClave, listaValor)
+    val mapa = fabricarDiccionario(listaClave, listaValor)
     println(mapa.keys)
     println(mapa.values)
+    println("La clave 1 está asociada con el valor ${mapa.get(1)}")
 }
 
 fun fabricarDiccionario(listaClave: List<Int>, listaValor: List<String>): Map<Int, String> {
