8e1fb0833ae8d098d497ea987ecda8509e849f1a
Jose <143912233+JoseJimenezGonzalez@users.noreply.github.com>
2023-11-02 20:12:31 +0100

    update regex

--- seminario/src/main/kotlin/ejercicio29/Main.kt
+++ seminario/src/main/kotlin/ejercicio29/Main.kt
@@ -2,23 +2,26 @@ package ejercicio29
 //Crea una función que determine si una cadena de texto es un
 //anagrama de otra cadena. Dos palabras son anagramas si tienen las mismas letras,
 //pero en un orden diferente.
-fun main(){
-    val texto1 = "Alborota"
-    val texto2 = "Rata, lobo"
-    println("¿$texto1 y $texto2 son anagramas? ${esAnagrama(texto1, texto2)}")
+fun sonAnagramas(cadena1: String, cadena2: String): Boolean {
+
+    val cadenaFormateada1 = cadena1.replace("[^A-Za-z]".toRegex(), "").toLowerCase()
+    val cadenaFormateada2 = cadena2.replace("[^A-Za-z]".toRegex(), "").toLowerCase()
+
+
+    if (cadenaFormateada1.length != cadenaFormateada2.length) {
+        return false
+    }
+
+
+    return cadenaFormateada1.toCharArray().sorted() == cadenaFormateada2.toCharArray().sorted()
 }
 
-fun esAnagrama(texto1: String, texto2: String): Boolean{
-    var res = false
-    val textoLimpio1 = texto1.replace("[^a-zA-Z0-9]".toRegex(), "").replace("\\s".toRegex(), "")
-    val textoLimpio2 = texto2.replace("[^a-zA-Z0-9]".toRegex(), "").replace("\\s".toRegex(), "")
-    if(textoLimpio1 != textoLimpio2 && textoLimpio1.length == textoLimpio2.length){
-        res = true
-        textoLimpio1.forEach { caracter ->
-            if(!texto2.contains(caracter, ignoreCase = true)){
-                res = false
-            }
-        }
-    }
-    return res
+fun main() {
+    val texto1 = "Alborota"
+    val texto2 = "Rata, lobo"
+    val texto3 = "hello"
+    val texto4 = "world"
+
+    println("¿'Alborota' y 'Rata, lobo' son anagramas? ${sonAnagramas(texto1, texto2)}")
+    println("¿'hello' y 'world' son anagramas? ${sonAnagramas(texto3, texto4)}")
 }
